a147e2bcae995dcb760e25e0a8c81a8972c7a58d
charry0071 <charry0071@gmail.com>
2023-04-12 18:32:36 +0700

    init

---
+++ wap/src/axios/api.url.js
@@ -0,0 +1,10 @@
+
+const ENV = process.env.NODE_ENV
+export default {
+  // DOMAIN: 'http://124.222.215.8:7999/stock2c1',
+  DOMAIN: 'http://localhost:8080/stock2c1_war_exploded',
+  baseURL: 'http://localhost:8080/stock2c1_war_exploded',
+  util: {
+    image: '/util/image.html' // 图片上传
+  }
+}
---
+++ wap/src/page/dazong/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <foot></foot>
+  </div>
+</template>
+<script>
+import foot from '@/components/foot/foot'
+
+export default {
+  components: {
+    foot
+  }
+}
+</script>
