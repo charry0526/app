aa8964aaab00868a1307cd0be1f2d5c7254ef0d1
tian <708900585@qq.com>
2023-04-19 17:30:35 +0800

    完成新股第三个页面逻辑

---
+++ wap/src/utils/data.js
@@ -0,0 +1,375 @@
+export const CodeList = [
+  {
+    agentId:
+      1,
+    allProfitAndLose:
+      751700000,
+    buyOrderId:
+      '20230320093854',
+    buyOrderPrice:
+      166000,
+    buyOrderTime:
+      1679278849000,
+    id:
+      2410,
+    marginAdd:
+      0,
+    nickName:
+      '哇打我',
+    now_price:
+      '',
+    orderDirection:
+      '买涨',
+    orderFee:
+      8300000,
+    orderLever:
+      5,
+    orderNum:
+      50000,
+    orderSpread:
+      0,
+    orderStayDays:
+      16,
+    orderStayFee:
+      0,
+    orderTotalPrice:
+      8300000000,
+    positionSn:
+      '16792799341679279934',
+    positionType:
+      0,
+    profitAndLose:
+      760000000,
+    profitTargetPrice:
+      null,
+    sellOrderId:
+      '2023040433347',
+    sellOrderPrice:
+      181200,
+    sellOrderTime:
+      1680575212000,
+    spreadRatePrice:
+      0,
+    stockCode:
+      'SAB',
+    stockGid:
+      'upcomSAB',
+    stockName:
+      'SABECO',
+    stockPlate:
+      '越股',
+    stockSpell:
+      'SABECO',
+    stopTargetPrice:
+      null,
+    userId:
+      943,
+    zt:
+      1
+  },
+  {
+    agentId:
+      1,
+    allProfitAndLose:
+      751700000,
+    buyOrderId:
+      '20230320093854',
+    buyOrderPrice:
+      166000,
+    buyOrderTime:
+      1679278849000,
+    id:
+      2410,
+    marginAdd:
+      0,
+    nickName:
+      '哇打我',
+    now_price:
+      '',
+    orderDirection:
+      '买涨',
+    orderFee:
+      8300000,
+    orderLever:
+      5,
+    orderNum:
+      50000,
+    orderSpread:
+      0,
+    orderStayDays:
+      16,
+    orderStayFee:
+      0,
+    orderTotalPrice:
+      8300000000,
+    positionSn:
+      '16792799341679279934',
+    positionType:
+      0,
+    profitAndLose:
+      760000000,
+    profitTargetPrice:
+      null,
+    sellOrderId:
+      '2023040433347',
+    sellOrderPrice:
+      181200,
+    sellOrderTime:
+      1680575212000,
+    spreadRatePrice:
+      0,
+    stockCode:
+      'SAB',
+    stockGid:
+      'upcomSAB',
+    stockName:
+      'SABECO',
+    stockPlate:
+      '越股',
+    stockSpell:
+      'SABECO',
+    stopTargetPrice:
+      null,
+    userId:
+      943,
+    zt:
+      1
+  }, {
+    agentId:
+      1,
+    allProfitAndLose:
+      751700000,
+    buyOrderId:
+      '20230320093854',
+    buyOrderPrice:
+      166000,
+    buyOrderTime:
+      1679278849000,
+    id:
+      2410,
+    marginAdd:
+      0,
+    nickName:
+      '哇打我',
+    now_price:
+      '',
+    orderDirection:
+      '买涨',
+    orderFee:
+      8300000,
+    orderLever:
+      5,
+    orderNum:
+      50000,
+    orderSpread:
+      0,
+    orderStayDays:
+      16,
+    orderStayFee:
+      0,
+    orderTotalPrice:
+      8300000000,
+    positionSn:
+      '16792799341679279934',
+    positionType:
+      0,
+    profitAndLose:
+      760000000,
+    profitTargetPrice:
+      null,
+    sellOrderId:
+      '2023040433347',
+    sellOrderPrice:
+      181200,
+    sellOrderTime:
+      1680575212000,
+    spreadRatePrice:
+      0,
+    stockCode:
+      'SAB',
+    stockGid:
+      'upcomSAB',
+    stockName:
+      'SABECO',
+    stockPlate:
+      '越股',
+    stockSpell:
+      'SABECO',
+    stopTargetPrice:
+      null,
+    userId:
+      943,
+    zt:
+      1
+  }, {
+    agentId:
+      1,
+    allProfitAndLose:
+      751700000,
+    buyOrderId:
+      '20230320093854',
+    buyOrderPrice:
+      166000,
+    buyOrderTime:
+      1679278849000,
+    id:
+      2410,
+    marginAdd:
+      0,
+    nickName:
+      '哇打我',
+    now_price:
+      '',
+    orderDirection:
+      '买涨',
+    orderFee:
+      8300000,
+    orderLever:
+      5,
+    orderNum:
+      50000,
+    orderSpread:
+      0,
+    orderStayDays:
+      16,
+    orderStayFee:
+      0,
+    orderTotalPrice:
+      8300000000,
+    positionSn:
+      '16792799341679279934',
+    positionType:
+      0,
+    profitAndLose:
+      760000000,
+    profitTargetPrice:
+      null,
+    sellOrderId:
+      '2023040433347',
+    sellOrderPrice:
+      181200,
+    sellOrderTime:
+      1680575212000,
+    spreadRatePrice:
+      0,
+    stockCode:
+      'SAB',
+    stockGid:
+      'upcomSAB',
+    stockName:
+      'SABECO',
+    stockPlate:
+      '越股',
+    stockSpell:
+      'SABECO',
+    stopTargetPrice:
+      null,
+    userId:
+      943,
+    zt:
+      1
+  }
+]
+export const CodeDateFormat = [{
+  label: '三三重工',
+  key: 'stockName',
+  value: '控股代码'
+},
+{
+  label: '利润总额',
+  key: 'allProfitAndLose',
+  value: '-100',
+  iscolor: true
+}, {
+  label: '购买价格',
+  key: 'buyOrderPrice',
+  value: '2000'
+}, {
+  label: '市场价',
+  key: 'orderTotalPrice',
+  value: '2000'
+}, {
+  label: '价格',
+  key: 'sellOrderPrice',
+  value: '2000'
+}, {
+  label: 'GD费用',
+  key: 'orderFee',
+  value: '2000'
+}, {
+  label: '举办天数',
+  key: 'orderStayDays',
+  value: '2000'
+}, {
+  label: '资本成本',
+  key: 'orderTotalPrice',
+  value: '2000'
+}, {
+  label: '数量',
+  key: 'orderNum',
+  value: '2000'
+}]
+export const CodeDateFormatFrist = [
+  {
+    label: '三三重工',
+    key: 'stockName',
+    value: '控股代码'
+  },
+  {
+    label: '最新价格',
+    key: 'now_price',
+    value: '2000',
+    iscolor: true
+  }, {
+    label: '购买价格',
+    key: 'buyOrderPrice',
+    value: '2000'
+  }, {
+    label: '市场价',
+    key: 'orderTotalPrice',
+    value: '2000'
+  }, {
+    label: '利润',
+    key: 'profitAndLose',
+    value: '2000',
+    iscolor: true
+  }, {
+    label: '数量',
+    key: 'orderNum',
+    value: '2000'
+  }
+
+]
+export const endorseList = [
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1002', fxtime: '2023-04-16 00:00:00', lever: '1', names: 'admin', newlist_id: 10, num: 10000, price: '9888', scprice: '900', zt: 2, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 },
+  { code: '1001', fxtime: '2023-04-15 00:00:00', lever: '1/5', names: 'test', newlist_id: 9, num: 1000, price: '8888', scprice: '8000', zt: 1, bzj: 1000 }
+]
