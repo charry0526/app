fd9015f5ffb1147937fb6d99bc8af97adcb02ace
tian <708900585@qq.com>
2023-05-12 21:19:13 +0800

    修改字段

--- wap/src/page/user/esop.vue
+++ wap/src/page/user/esop.vue
@@ -28,9 +28,10 @@
             <tr v-for="(item,index) in stockList"
                 :key="index">
               <td>{{item.names}}</td>
-              <td>{{$moneyDot(item.scprice)}}</td>
-              <!-- <td>{{$moneyDot(item.price)}}</td> -->
+              <!-- <td>{{$moneyDot(item.scprice)}}</td> -->
               <td>{{$moneyDot(item.nowPrice)||''}}</td>
+              <td>{{$moneyDot(item.price)}}</td>
+              <!-- <td>{{$moneyDot(item.nowPrice)||''}}</td> -->
               <td>
                 <div class="button-box">
                   <mt-button class="btn-red pull-right"
@@ -181,19 +182,15 @@ export default {
   },
   computed: {
     deposit () {
-      const { price, nowPrice } = this.itemInfo
-      if (nowPrice) {
-        return this.selectNumber * nowPrice / this.leverValue
-      } else {
-        return this.selectNumber * price / this.leverValue
-      }
+      const { price } = this.itemInfo
+      return this.selectNumber * price / this.leverValue
     }
   },
   methods: {
     // 获取市场价格
     async getactualPrice (option) {
       let opts = {
-        code: option.code
+        code: option.names
       }
       let res = await api.getSingleStock(opts)
       const data = res.data
@@ -272,7 +269,7 @@ export default {
         return this.$message.warning('Tối thiểu cần' + this.itemInfo.num)
       }
       const { agentName, realName, phone } = this.userInfo
-      const { zt, code, names, scprice, nowPrice } = this.itemInfo
+      const { zt, code, names, scprice, price } = this.itemInfo
       const option = {
         agent: agentName, // 代理
         zname: realName, // 真实名称
@@ -282,9 +279,10 @@ export default {
         // codes: code, // 新股代码
         nums: this.selectNumber, // 数量
         bzj: this.deposit, // 保证金
+        // price: price,
         gg: this.leverValue, // 杠杆
-        // sz: Number(this.selectNumber) * Number(price), // 市值
-        sz: Number(this.selectNumber) * Number(nowPrice), // 市值
+        sz: Number(this.selectNumber) * Number(price), // 市值
+        // scprice: scprice,
         mrsj: formatTime() // 提交日期
       }
       try {
